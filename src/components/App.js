43e6898c0b7895202c6abdad2cbcaf7193f0b76f
Samarjeet <samarjeetpandey1402@gmail.com>
2024-07-21 11:38:14 +0530

    assignment submission update8

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ const App = () => {
     <div>
       <p>Enter Your name:</p>
       <input type="text" onChange={handleInput}></input>
-      {input ? <span>Hello {input}!</span> : null}
+      {input ? <p>Hello {input}!</p> : null}
     </div>
   );
 };
